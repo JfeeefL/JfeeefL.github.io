81f355d54eb7e7fc7f1e35d8182a5f2ecc7921a5
Jeff Lee <1531488917@qq.com>
2021-02-06 23:55:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const b = document.querySelectorAll('button');
+for(var i = 0; i < b.length; i ++) {
+    b[i].addEventListener('click', echo);
+}
+
+function echo() {
+    var p = document.createElement('button');
+    p.textContent = 'Click Me';
+    document.body.appendChild(p);
+}
