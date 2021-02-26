058b033eea54c10b74d4e254841c50061812e48c
Jeff Lee <1531488917@qq.com>
2021-02-26 21:08:20 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,96 +1,125 @@
+//Library code:
 const canvas = document.querySelector('canvas');
 const device = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const stageWidth = canvas.width = window.innerWidth;
+const stageHeight = canvas.height = window.innerHeight;
+
+let ActorList = [];
+
+function Actor (attribute, renderer, matrix) {
+    this.attribute = attribute;
+    this.renderer = renderer;
+    this.matrix = matrix;
+};
+
+let activate = function (actor) {
+    ActorList.push(new Actor(actor.attribute, actor.renderer, actor.matrix));
+    ActorList[ActorList.length-1].attribute.id = ActorList.length-1;
+}
+
+function mainRenderer() {
+    for(let i = 0; i < ActorList.length; i ++) {
+        ActorList[i].matrix(ActorList[i].attribute);
+    }
+    device.clearRect(0,0, stageWidth, stageHeight);
+    for(let i = 0; i < ActorList.length; i ++) {
+        ActorList[i].renderer(ActorList[i].attribute);
+    }
+}
+
+const timeInterval = 10;
+
+setInterval(mainRenderer,timeInterval);
+//Library code end
+
+
+//-------------------------------------------------------------------------------------
+//user's code:
 
 function vec2D(x, y) {
     return {x:x, y:y};
 }
 
-function PhyAtt(pos, velocity, color) {
-    return {pos : pos, velocity : velocity, color: color};
+function sqr(x) {return x*x;}
+
+function distance(p1, p2) {
+    return Math.sqrt(sqr(p1.x-p2.x)+sqr(p1.y-p2.y));
 }
 
-let renderList = [];
-let deltaList = [];
+function dot(p1, p2) {
+    return p1.x*p2.x + p1.y*p2.y;
+}
 
-function ObjectRenderer(func,ini) {
-    this.data = ini;
-    this.core = func;
-    this.render = function() {
+function multiply(p, x) {
+    return {x: p.x*x, y: p.y*x};
+}
+
+function plus(a, b) {
+    return {x: a.x + b.x, y: a.y + b.y};
+}
+
+function minus(a, b) {
+    return {x: a.x - b.x, y: a.y - b.y};
+}
+
+function assign(a, b) {
+    a.x = b.x;
+    a.y = b.y;
+}
+
+function normalize(p) {
+    assign(p,multiply(p,1/distance(vec2D(0,0), p)));
+}
+
+//------------------------------------------------------------------
+function reflect(p, normal) {
+    let middle = multiply(normal, dot(p, normal));
+    assign(p, plus(middle, minus(middle, p)));
+}
+
+function ballCollision(b1, b2, col) {
+    if(b1.r + b2.r < distance(b1.p, b2.p)) return false;
+    if(col) return true;
+    let normal = minus(b1.p, b2.p);
+    normalize(normal);
+    reflect(b1.v, normal);
+    return true;
+}
+
+let Ball = new Actor(
+    null,
+    function(attr) {
         device.beginPath();
-        this.core(this.data);
+        device.arc(attr.p.x, attr.p.y, attr.r,0,2*Math.PI);
+        device.fillStyle = attr.color;
+        device.fill();
         device.closePath();
+    },
+    function (attr) {
+        assign(attr.p, plus(attr.p,attr.v));
+        if(attr.p.x - attr.r < 0 || attr.p.x + attr.r > stageWidth) attr.v.x *= -1;
+        if(attr.p.y - attr.r < 0 || attr.p.y + attr.r > stageHeight)attr.v.y *= -1;
+        for(let i = 0; i < ActorList.length; i ++) {
+            if(attr.id === i) continue;
+            if(ballCollision(attr, ActorList[i].attribute, attr.collide.has(i))) attr.collide.add(i);
+            else attr.collide.delete(i);
+        }
     }
-
-}
-
-function ObjectDelta(func,ini) {
-    this.data = ini;
-    this.delta = func;
-}
-
-function GameObject() {
-    this.render = null;
-    this.delta = null;
-    this.ini = null;
-    this.activate = function() {
-        console.log(this.ini);
-        renderList.push(new ObjectRenderer(this.render,this.ini));
-        deltaList.push(new ObjectDelta(this.delta,this.ini));
-    }
-}
-
-function mainRenderer() {
-    device.clearRect(0,0,canvas.width,canvas.height);
-    for(let i = 0; i < deltaList.length; i ++) {
-        renderList[i].data = deltaList[i].data = deltaList[i].delta(deltaList[i].data);
-    }
-    for(let i = 0; i < renderList.length; i ++) {
-        renderList[i].render(renderList[i].data);
-    }
-}
-
-let Ball = new GameObject;
-Ball.render = function (obj) {
-    device.arc(obj.pos.x, obj.pos.y, 10, 0, 2* Math.PI);
-    device.fillStyle = obj.color;
-    device.fill();
-};
-Ball.delta = function(obj) {
-    obj.pos.x += obj.velocity.x;
-    obj.pos.y += obj.velocity.y;
-    if(obj.pos.x < 0) {
-        obj.pos.x = 0;
-        obj.velocity.x *= -1;
-    } else if(obj.pos.x > width){
-        obj.pos.x = width;
-        obj.velocity.x *= -1;
-    }
-
-    if(obj.pos.y < 0) {
-        obj.pos.y = 0;
-        obj.velocity.y *= -1;
-    } else if(obj.pos.y > height){
-        obj.pos.y = height;
-        obj.velocity.y *= -1;
-    }
-    return obj;
-};
+);
 
 function random(l, r) {
-    return l+Math.floor((r-l)*Math.random());
+    return l+(r-l)*Math.random();
 }
 
-for(let i = 0; i < 30; i ++) {
-    Ball.ini = PhyAtt(
-        vec2D(random(0,width), random(0,height)),
-        vec2D(random(-5, 5), random(-5, 5)),
-        "rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")"
-    );
-    Ball.activate();
+for(let i = 0; i < 50; i ++) {
+    Ball.attribute = {
+        p:vec2D(random(0+20,stageWidth-20), random(0+20,stageHeight-20)),
+        v:vec2D(random(-2,2), random(-2,2)),
+        r: random(10,20),
+        color: 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')',
+        collide: new Set()
+    };
+    activate(Ball);
 }
 
-
-setInterval(mainRenderer,10);
