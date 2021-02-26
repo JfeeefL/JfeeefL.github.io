f9e5614436ee1beea4aebc304cc76fb49ee5ea2d
Jeff Lee <1531488917@qq.com>
2021-02-27 00:35:10 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,125 +1,102 @@
-//Library code:
 const canvas = document.querySelector('canvas');
 const device = canvas.getContext('2d');
 
 const stageWidth = canvas.width = window.innerWidth;
 const stageHeight = canvas.height = window.innerHeight;
 
-let ActorList = [];
-
-function Actor (attribute, renderer, matrix) {
+function FrameActor(attribute, updater, renderer) {
     this.attribute = attribute;
+    this.updater = updater;
     this.renderer = renderer;
-    this.matrix = matrix;
-};
+    this.id = 0;
+}
 
-let activate = function (actor) {
-    ActorList.push(new Actor(actor.attribute, actor.renderer, actor.matrix));
-    ActorList[ActorList.length-1].attribute.id = ActorList.length-1;
+let frameActorList = [];
+
+function activate (frameActor) {
+    frameActorList.push(frameActor);
 }
 
 function mainRenderer() {
-    for(let i = 0; i < ActorList.length; i ++) {
-        ActorList[i].matrix(ActorList[i].attribute);
-    }
-    device.clearRect(0,0, stageWidth, stageHeight);
-    for(let i = 0; i < ActorList.length; i ++) {
-        ActorList[i].renderer(ActorList[i].attribute);
-    }
+    for(let i = 0; i < frameActorList.length; i ++)
+        frameActorList[i].updater(frameActorList[i].attribute);
+    device.clearRect(0,0, stageWidth,stageHeight);
+    for(let i = 0; i < frameActorList.length; i ++)
+        frameActorList[i].renderer(frameActorList[i].attribute);
 }
 
-const timeInterval = 10;
+setInterval(mainRenderer, 10);
 
-setInterval(mainRenderer,timeInterval);
-//Library code end
+//====================| vec2D |=======================//
 
-
-//-------------------------------------------------------------------------------------
-//user's code:
-
-function vec2D(x, y) {
-    return {x:x, y:y};
+function Vec2D(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
-function sqr(x) {return x*x;}
+//====================| vec2D END |=======================//
 
-function distance(p1, p2) {
-    return Math.sqrt(sqr(p1.x-p2.x)+sqr(p1.y-p2.y));
+let arrowVec = new Vec2D(0,0);
+
+//====================| MOUSE ACTOR : ID = 0 |=======================//
+
+let mouseAttribute = {
+    pos: arrowVec,
+    maxR: 20,
+    minR: 15,
+    r: 15,
+    inc: 0.1,
+    id : 0
+};
+
+function mouseUpdater (attr) {
+    attr.R += attr.inc;
+    if(attr.R > attr.maxR || attr.r < attr.minR) attr.inc*=-1;
 }
 
-function dot(p1, p2) {
-    return p1.x*p2.x + p1.y*p2.y;
+function mouseRenderer(attr) {
+    device.beginPath();
+    device.arc(attr.pos.x, attr.pos.y, attr.r, 0, Math.PI * 2);
+    device.strokeStyle = 'rgba(50, 100, 100, 0.5)';
+    device.stroke();
+    device.closePath();
 }
 
-function multiply(p, x) {
-    return {x: p.x*x, y: p.y*x};
+activate(new FrameActor(mouseAttribute, mouseUpdater, mouseRenderer));
+
+//====================| MOUSE ACTOR END : ID = 0 |=======================//
+
+//====================| ARROW ACTOR : ID = 1 |=======================//
+
+let arrowAttribute = {
+    start: new Vec2D(0,0),
+    end: arrowVec,
+    id: 1
+};
+
+function arrowRenderer(attr) {
+    device.beginPath();
+    device.moveTo(attr.start.x, attr.start.y);
+    device.lineTo(attr.end.x, attr.end.y);
+    device.stroke();
+    device.closePath();
 }
 
-function plus(a, b) {
-    return {x: a.x + b.x, y: a.y + b.y};
-}
+activate(new FrameActor(arrowAttribute, function() {}, function () {}));
 
-function minus(a, b) {
-    return {x: a.x - b.x, y: a.y - b.y};
-}
+//====================| ARROW ACTOR END : ID = 1 |=======================//
 
-function assign(a, b) {
-    a.x = b.x;
-    a.y = b.y;
-}
+document.addEventListener('mousemove', function(e) {
+    arrowVec.x = e.clientX-canvas.offsetLeft;
+    arrowVec.y = e.clientY-canvas.offsetTop;
+});
 
-function normalize(p) {
-    assign(p,multiply(p,1/distance(vec2D(0,0), p)));
-}
+document.addEventListener('mousedown', function (e) {
+    arrowAttribute.start.x = arrowVec.x;
+    arrowAttribute.start.y = arrowVec.y;
+    frameActorList[1].renderer = arrowRenderer;
+});
 
-//------------------------------------------------------------------
-function reflect(p, normal) {
-    let middle = multiply(normal, dot(p, normal));
-    assign(p, plus(middle, minus(middle, p)));
-}
-
-function ballCollision(b1, b2, col) {
-    if(b1.r + b2.r < distance(b1.p, b2.p)) return false;
-    if(col) return true;
-    let normal = minus(b1.p, b2.p);
-    normalize(normal);
-    reflect(b1.v, normal);
-    return true;
-}
-
-let Ball = new Actor(
-    null,
-    function(attr) {
-        device.beginPath();
-        device.arc(attr.p.x, attr.p.y, attr.r,0,2*Math.PI);
-        device.fillStyle = attr.color;
-        device.fill();
-        device.closePath();
-    },
-    function (attr) {
-        assign(attr.p, plus(attr.p,attr.v));
-        if(attr.p.x - attr.r < 0 || attr.p.x + attr.r > stageWidth) attr.v.x *= -1;
-        if(attr.p.y - attr.r < 0 || attr.p.y + attr.r > stageHeight)attr.v.y *= -1;
-        for(let i = 0; i < ActorList.length; i ++) {
-            if(attr.id === i) continue;
-            if(ballCollision(attr, ActorList[i].attribute, attr.collide.has(i))) attr.collide.add(i);
-            else attr.collide.delete(i);
-        }
-    }
-);
-
-function random(l, r) {
-    return l+(r-l)*Math.random();
-}
-
-for(let i = 0; i < 50; i ++) {
-    Ball.attribute = {
-        p:vec2D(random(0+20,stageWidth-20), random(0+20,stageHeight-20)),
-        v:vec2D(random(-2,2), random(-2,2)),
-        r: random(10,20),
-        color: 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')',
-        collide: new Set()
-    };
-    activate(Ball);
-}
-
+document.addEventListener('mouseup', function (e) {
+    frameActorList[1].renderer = function () {};
+});
